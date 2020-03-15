27cce6592f57668ae4481e7000204ce7c7a62e70
Amir Yunas <ayunas@gmail.com>
2020-03-15 00:10:43 -0400

    started the header of the portfolio using style.scss

--- src/components/header.js
+++ src/components/header.js
@@ -2,23 +2,31 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import './style.scss';
+import bitmoji from '../images/bitmoji.png';
+console.log(bitmoji);
 
 const Header = ({ siteTitle }) => (
   <header>
-    <div>
-      <h1>
+      <div class="title">
+        <img class="bitmoji" src={bitmoji} alt="bitmoji selfie" />
         <Link to="/">{siteTitle}</Link>
-      </h1>
-    </div>
+      </div>
+
+      <div class="links">
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+
   </header>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"  
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <h1>Welcome!</h1>
+    <h2>I'm Amir</h2>
+    <h2>A full stack developer</h2>
+    <h2>I code... I cloud...</h2>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div>
+    </div> */}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
