7e330dd078a09f4d006224862a1a5c08e77cb28e
Amir Yunas <ayunas@gmail.com>
2020-03-24 15:11:07 -0400

    used require.context to build a static context of images prior to running the component.  then called it passing in the image name and it returned the image for use in the component.

--- src/components/project.js
+++ src/components/project.js
@@ -1,14 +1,20 @@
 import React from 'react';
 // import {Link} from 'gatsby';
 // import Img from 'gatsby-image';
+const images = require.context('../images', true);
+//https://webpack.js.org/guides/dependency-management/  / https://github.com/survivejs/webpack-book/issues/80
+//dynamically importing images, using the context method of require.  input the directory you want to use, and it returns a function that allows you to query the newly created static context for the image.  use './<file_name>'  to query for the image.  true 2nd argument for including subdirectories in the context.  
 
+const Project = ({title,tech,description,image}) =>{
 
-const Project = ({title,tech,description,image}) =>{ 
     console.log(image);
+    const img_src = images(`./${image}`);
+    console.log(img_src);
     
     return (
         <section className="project">
-            <img src={`${image}`}/>  
+            <img src={img_src}/>
+            {/* <img src={require(`${image}`)}/> */}
             <div className="project-info">
                 <h3>{title}</h3>
                 <p>{tech}</p>
