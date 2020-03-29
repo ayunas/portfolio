bf779b1707960023c665ed6ff159473d9ff05e6f
Amir Yunas <ayunas@gmail.com>
2020-03-28 23:12:55 -0400

    added social-media-icons-react. aligned text in each project and correct alignment

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-// import {Link} from 'gatsby';
-// import Img from 'gatsby-image';
+import {SocialMediaIconsReact as Social} from 'social-media-icons-react';
 const images = require.context('../images', true);
 //https://webpack.js.org/guides/dependency-management/  / https://github.com/survivejs/webpack-book/issues/80
 //dynamically importing images, using the context method of require.  input the directory you want to use, and it returns a function that allows you to query the newly created static context for the image.  use './<file_name>'  to query for the image.  true 2nd argument for including subdirectories in the context.  
 
-const Project = ({title,tech,description,image}) =>{
+const Project = ({title,tech,description,image,github}) =>{
 
     console.log(image);
     const img_src = images(`./${image}`);
@@ -17,8 +16,15 @@ const Project = ({title,tech,description,image}) =>{
             {/* <img src={require(`${image}`)}/> */}
             <div className="project-info">
                 <h3>{title}</h3>
+                <br/>
                 <p>{description}</p>
-                <ul>Tech Stack : {tech.map(t => <li>{t}</li>)}</ul>
+                <br/>
+                <h4>Tech Stack: </h4>
+                <ul>{tech.map(t => <li>{t}</li>)}</ul>
+                <figure>
+                    <Social icon="github" size={40} iconSize={10} roundness={20} url={github} />
+                    {/* <figcaption>code</figcaption> */}
+                </figure>
             </div>
         </section>
     )}
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -14,7 +14,7 @@ const ProjectsPage = () => {
   return (
       <Layout>
         <section className="projects">
-          {projectData.map(p => <Project title={p.title} tech={p.tech} description={p.description} image={p.image} />)}
+          {projectData.map(p => <Project title={p.title} tech={p.tech} description={p.description} image={p.image} github={p.github}/>)}
           {/* <Project title="project title" tech="JS" description="cool project!" imagePath={"https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-260nw-721703848.jpg"}/>
           <Project title="2nd project" tech="Python" description="cool project!" imagePath={"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"}/> */}
         </section>
