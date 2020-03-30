0c572c174a61a6626990798f57d5116cd4f39bba
Amir Yunas <ayunas@gmail.com>
2020-03-30 02:23:30 -0400

    added blockchain screenshot to project component

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -29,17 +29,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      {/* <Test/> */}
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
         <main>{children}</main>
       <Footer/> 
-      </div>
     </>
   )
 }
